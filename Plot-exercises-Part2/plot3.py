e81c3a296ec370bf747717f6385b631c0108e59f
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-23 11:40:04 +0700

    3rd plot exercise done

--- Plot-exercises-Part2/plot3.py
+++ Plot-exercises-Part2/plot3.py
@@ -6,18 +6,18 @@ def plot_funtion():
     y = np.linspace(-3, 3, 30)
     X,Y = np.meshgrid(x,y)
 
-    Z = X * Y
-    
+    Z =  np.sin(3*X)*Y
+
     fig= plt.figure(figsize =(10,6))
     ax = fig.add_subplot(111, projection='3d')
 
     surf = ax.plot_surface(X, Y, Z, cmap='inferno', edgecolor='none')
-    fig.colorbar(surf, ax=ax, label='f(x, y) = x * y')
+    fig.colorbar(surf, ax=ax, label='f(x, y) = sin(3*x)*y')
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('f(x, y)')
-    ax.set_title('Surface Plot of f(x, y) = x * y')
+    ax.set_title('Surface Plot of f(x, y) = sin(3*x)*y')
 
     plt.show()
 
