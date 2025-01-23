1f23f6216e0639e45d839e6c138f370c2f5cd8af
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-23 10:02:27 +0700

    First plot done

---
+++ Plot-exercises/plot1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_funtion():
+    x = np.linspace(-10,10, 500)
+    y = x**2
+
+    plt.figure(figsize =(10,6))
+    plt.plot(x,y, color = "green", label="$f(x) = x^2$")
+
+    plt.legend()
+    plt.title('Graph of the Function $f(x) = x^2$')
+    plt.xlabel('x-axis')
+    plt.ylabel('f(x)')
+    plt.grid(True)
+
+    plt.show()
+
+plot_funtion()
